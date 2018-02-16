cd6fbf334e24fbeb365831dabde4ee3e70eab042
Kia Farhang <kfarhang0@gmail.com>
2018-02-16 15:20:44 -0600

    Add an interface for the requests that come from Alexa

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,14 @@
+import { Context, IntentRequest, LaunchRequest, Session, SessionEndedRequest } from "alexa-app";
+
 export interface Story {
     text: string;
     imageURL: string;
+    storyURL: string;
+}
+
+export interface AlexaRequest {
+    version: string;
+    session: Session;
+    context: Context;
+    request: IntentRequest | LaunchRequest | SessionEndedRequest;
 }
