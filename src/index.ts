ceeccf8f4168c8d6776e0174d169282dd41adde6
Kia Farhang <kfarhang0@gmail.com>
2018-02-16 15:20:12 -0600

    Run Alexa via Express from top-level index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-// tslint:disable-next-line
-console.log('Hi test');
+import alexaApp from "./alexa";
+import { Story } from "./interfaces";
+import expressApp from "./server";
+
+// tslint:disable-next-line: no-var-requires
+// const stories: Story[] = require("./stories.json");
+
+alexaApp.express({
+    expressApp,
+
+    // verifies requests come from amazon alexa. Must be enabled for production.
+    // You can disable this if you're running a dev environment and want to POST
+    // things to test behavior. enabled by default.
+    checkCert: false,
+});
+
+export default expressApp.listen(5000);
